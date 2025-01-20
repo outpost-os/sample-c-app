5729cfe760a5021651a77a11e504ff6974846454
Florent Valette <florent.valette@ledger.fr>
2025-01-20 11:14:36 +0100

    initial commit

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2023 Ledger SAS
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, World from sample C app\n");
+    return 0;
+}
